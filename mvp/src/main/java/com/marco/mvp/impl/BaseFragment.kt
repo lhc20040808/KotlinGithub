3ff1c319c828ba53dbc9bbe2b85bc3f7041794ea
lhc <marco.li@ximalaya.com>
2021-07-12 22:59:59 +0800

    [feat]createPresenter方法实现

--- mvp/src/main/java/com/marco/mvp/impl/BaseFragment.kt
+++ mvp/src/main/java/com/marco/mvp/impl/BaseFragment.kt
@@ -3,6 +3,10 @@ package com.marco.mvp.impl
 import androidx.fragment.app.Fragment
 import com.marco.mvp.IMVPView
 import com.marco.mvp.IPresenter
+import kotlin.reflect.KClass
+import kotlin.reflect.full.isSubclassOf
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.jvmErasure
 
 abstract class BaseFragment<out P : BasePresenter<BaseFragment<P>>> : IMVPView<P>, Fragment() {
 
@@ -13,5 +17,19 @@ abstract class BaseFragment<out P : BasePresenter<BaseFragment<P>>> : IMVPView<P
         presenter.view = this
     }
 
-    fun createPresenter(): P = TODO()
+    private fun createPresenter(): P {
+        sequence {
+            var thisClass: KClass<*> = this@BaseFragment::class
+            while (true) {
+                yield(thisClass.supertypes)
+                thisClass = thisClass.supertypes.firstOrNull()?.jvmErasure ?: break
+            }
+        }.flatMap {
+            it.flatMap { arg -> arg.arguments }.asSequence()
+        }.first {
+            it.type?.jvmErasure?.isSubclassOf(IPresenter::class) ?: false
+        }.let {
+            return it.type!!.jvmErasure.primaryConstructor!!.call() as P
+        }
+    }
 }
